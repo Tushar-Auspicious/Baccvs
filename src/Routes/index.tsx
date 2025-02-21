84f392d2717f24e5808b09e95d11f0f4e9f25b19
Tushar Thakur <tushar@auspicioussoft.com>
2025-02-21 15:51:58 +0530

    minor change

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -334,8 +334,8 @@ const Routing = () => {
         headerShown: false,
       }}
     >
-      <RootStack.Screen name="mainStack" component={MainStack} />
       <RootStack.Screen name="authStack" component={AuthStack} />
+      <RootStack.Screen name="mainStack" component={MainStack} />
     </RootStack.Navigator>
   );
 };
